65ef59a3b372c4030e01f44b30f9b1fab4074567
Andrew Dowell <adowellphoto@gmail.com>
2015-11-03 18:10:51 +0000

    resubmitted

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,6 +6,8 @@ class Tag
     property :tags, String
 end
 
+#
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
