c0ec1c649d628a5d0b62dd84bb30407cc380d5bd
jamierose <jrose111284@gmail.com>
2015-11-06 12:20:07 +0000

    chapter 22

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ require 'dm-validations'
 class User
   include DataMapper::Resource
     property :id, Serial
-    property :email, String, required: true
+    property :email, String, required: true, unique: true
     property :password_digest, Text
     attr_reader :password
     attr_accessor :password_confirmation
